6a97013320e80c3bbe537f4832d9a119aee78d68
barsu5489 <ebarsulai@gmail.com>
2023-01-25 22:34:33 +0300

    Auth update

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,57 +1,53 @@
-
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-
-
-
 function Login() {
-    const navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
 
-    
-    
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch('http://localhost:3000/users');
+      const data = await res.json();
+      const user = data.find(user => user.email === email && user.password === password);
+      if (user) {
+        localStorage.setItem('token', user.token);
+        navigate('/signup');
+      } else {
+        setError('Invalid email or password');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const res = await fetch('http://localhost:3000/users', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ email, password }),
-            });
-            const data = await res.json();
-            localStorage.setItem('token', data.token);
-            navigate('/signup');
-        } catch (err) {
-            
-            console.log(err);
-        }
-    };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+      </label>
+      <br />
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      <button type="submit">Login</button>
+    </form>
+  );
+}
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Email:
-                <input
-                    type="email"
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                />
-            </label>
-            <br />
-            <label>
-                Password:
-                <input
-                    type="password"
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                    />
-                    </label>
-                    <br />
-                    <button type="submit">Login</button>
-                    </form>
-                    );
-                    }
- export default Login 
+export default Login;
